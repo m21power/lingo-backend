35dc32bc63aa30a39ea12e6ba39e149849728ec1
mesay <mesaylema21@gmail.com>
2025-06-20 10:36:07 +0300

    add: ssl mode

--- db/db.go
+++ db/db.go
@@ -27,9 +27,10 @@ func ConnectDb() (*sql.DB, error) {
 	dbname := os.Getenv("DB_NAME")
 	password := os.Getenv("DB_PASSWORD")
 	user := os.Getenv("DB_USER")
+	sslmode := os.Getenv("SSL_MODE")
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+		"password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
